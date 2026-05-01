cbf1d9a63279e101e0af6ba32c164d5d92d643c6
Jheyjaygq <ja33278@gmail.com>
2026-05-01 02:55:07 -0300

    feat: add static data arrays for app

---
+++ data/categoriasdata.ts
@@ -0,0 +1,23 @@
+
+export const categoriasData = [
+  {
+    id: "morango",
+    name: "Morango",
+    image: require("../assets/images/morango.png.jpg"),
+  },
+  {
+    id: "chocolate",
+    name: "Chocolate",
+    image: require("../assets/images/chocolate.png.jpg"),
+  },
+  {
+    id: "menta",
+    name: "Menta",
+    image: require("../assets/images/baunilha.png.jpg"),
+  },
+  {
+    id: "oreo",
+    name: "Oreo",
+    image: require("../assets/images/cupcake.png.jpg"),
+  },
+];
